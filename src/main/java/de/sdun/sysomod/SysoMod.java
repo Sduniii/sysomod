d4c7c09b106e7372a31059261fc58fb79bf78e02
tobia <tobias.sdun@gmail.com>
2019-01-11 14:43:36 +0100

    Initial commit

---
+++ src/main/java/de/sdun/sysomod/SysoMod.java
@@ -0,0 +1,58 @@
+package de.sdun.sysomod;
+/**
+ * Created by Tobias Sdun on 31.08.2017.
+ */
+public class SysoMod {
+
+    public static void loadSysoMod() {
+        System.setOut(new java.io.PrintStream(System.out) {
+
+            private StackTraceElement getCallSite() {
+                for (StackTraceElement e : Thread.currentThread()
+                        .getStackTrace())
+                    if (!e.getMethodName().equals("getStackTrace")
+                            && !e.getClassName().equals(getClass().getName()))
+                        return e;
+                return null;
+            }
+
+            @Override
+            public void println(String s) {
+                println((Object) s);
+            }
+
+            @Override
+            public void println(Object o) {
+                StackTraceElement e = getCallSite();
+                String fo = String.format("%-70s", o);
+                String callSite = e == null ? "??" :
+                        String.format("%s.%s(%s:%d)",
+                                e.getClassName(),
+                                e.getMethodName(),
+                                e.getFileName(),
+                                e.getLineNumber());
+                super.println(fo + "at " + callSite);
+            }
+
+            @Override
+            public void println(double x) {
+                println((Object) x);
+            }
+
+            @Override
+            public void println(int x) {
+                println((Object) x);
+            }
+
+            @Override
+            public void println(float x) {
+                println((Object) x);
+            }
+
+            @Override
+            public void println(long x) {
+                println((Object) x);
+            }
+        });
+    }
+}
